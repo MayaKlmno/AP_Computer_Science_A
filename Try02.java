76ce6d49cb461803ba7ab41530fe84c8d9533862
Maya Oniciuc <maya.klmno@gmail.com>
2023-10-02 21:07:18 -0700

    Learning about Strings

---
+++ Try02.java
@@ -0,0 +1,14 @@
+public class Try02 {
+    public static double calculate(double x){
+        return x + 1.5;
+    }
+
+    public static void main(String[] args){
+        int val =(int) Math.random() * 4 + 1 ;
+
+        val *= 2;
+        
+        System.out.print(val);
+
+    }
+}
---
+++ bank.java
@@ -0,0 +1,17 @@
+public class bank {
+    /*
+    Write an algorithm to settle the following question: A bank account starts out with $10,000. Interest is 
+    compounded monthly at 6 perscent per year (0.5 percent per month).
+    Evrey month, $ 500 is withdrawn to meet college expences. After how many years is the account depleted?
+     */
+    double balance;
+    public void withdraw(double amount){
+        balance = balance - amount;
+        boolean amount <= 0;
+    }
+
+    public static void main(String[] args){
+        
+    }
+
+}
